907d41c44e3766181d15a0b1697245163d8d502a
Mohammad Hamid <mohammad200h@hotmail.com>
2025-04-29 17:34:48 +0100

    updated

--- base_tile.py
+++ base_tile.py
@@ -4,104 +4,98 @@ import numpy as np
 
 from scipy.signal import convolve2d
 
-SAND_RGBA = [0.96, 0.96, 0.86, 1.0]
 
-box = {
-  "start":(0, 0),
-  "dim":(2, 2)
-}
-
-def reset_grid(grid,grid_size):
-  for i in range(grid_size[0]):
-    for j in range(grid_size[1]):
-      grid[i][j].pos[2] = 0
-
-def there_is_a_hole(grid,grid_size):
-  # Maybe replace this for loop with numpy
-  # Fill gaps caused by operation if any
-  # TODO:Go through each 4 neighbor if the neighbor
-  # abs(dz) is bigger than 2 * 0.1 move it up or down
-  for i in range(grid_size[0]):
-    for j in range(grid_size[1]):
-      # Looking at neighbors height
-      current_tile_z = grid[i][j].pos[2]
-      for k in [1,-1]:
-        for l in [1,-1]:
-          if i + k < grid_size[0]:
-            if j + l < grid_size[1]:
-              neighbors_tile_z = grid[i+k][j+l].pos[2]
-              dz = get_dz(current_tile_z , neighbors_tile_z, 0.1)
-              if abs(dz) > 0.25:
-                return True
-
-def get_dz(current_tile_z, neighbor_tile_z, thickness):
-  threshold = 5e-3
-  current  = {"top":current_tile_z + thickness, "bottom":current_tile_z - thickness}
-  neighbor = {"top":neighbor_tile_z + thickness, "bottom":neighbor_tile_z - thickness}
-
-  dist = 0
-  # neighbor is underneath
-  if neighbor["top"] < current["bottom"] + threshold:
-    dist = abs(current["bottom"] - neighbor["top"])
-
-  # neighbor is above
-  if neighbor["bottom"] > current["top"] + threshold:
-    dist = abs(neighbor["bottom"] - current["top"])
-
-  return dist
-
-def stairs(base_tile, num_stairs = 4, direction = 1):
-  SQUARE_LENGTH = 1 # singe square length
+def stairs(spec, num_stairs=4, direction=1, name='base_tile'):
+  SQUARE_LENGTH = 1
   THICKNESS = 0.1
   STEP = 0.1
 
+  BROWN_RGBA = [0.647, 0.165, 0.165, 1.0]
+
+  base_tile = spec.body(name)
   for i in range(num_stairs):
     # Side left
-    pos = [ SQUARE_LENGTH * 1 + STEP * (1 + 2 * i),
-           0,
-          direction * (0 + 2 * i) * THICKNESS]
-    size = [SQUARE_LENGTH * 0.1 , (2 - STEP * ( 2 * i)) * SQUARE_LENGTH, THICKNESS]
-    base_tile.add_geom(pos=pos, size=size, rgba = SAND_RGBA)
-
+    pos = [SQUARE_LENGTH * 1 + STEP * (1 + 2 * i), 0,
+           direction * (2 * i) * THICKNESS]
+    size = [SQUARE_LENGTH * 0.1 , (2 - STEP * ( 2 * i)) * SQUARE_LENGTH,
+            THICKNESS]
+    base_tile.add_geom(pos=pos, size=size, rgba=BROWN_RGBA)
     # Side right
-    pos = [SQUARE_LENGTH * 5 - STEP * (1 + 2 * i),
-           0,
-          direction * (0 + 2 * i) * THICKNESS]
-    base_tile.add_geom(pos=pos, size=size, rgba = SAND_RGBA)
-
+    pos = [SQUARE_LENGTH * 5 - STEP * (1 + 2 * i), 0,
+           direction * (2 * i) * THICKNESS]
+    base_tile.add_geom(pos=pos, size=size, rgba=BROWN_RGBA)
     # Bottom
-    pos =[3 * SQUARE_LENGTH ,
-          -(2 - STEP * (1 + 2 * i)) * SQUARE_LENGTH,
-          direction * (0 + 2 * i) * THICKNESS]
-    size =  [(2 - (2* STEP * (1+ i))) * SQUARE_LENGTH,
-                             SQUARE_LENGTH * 0.1,
-                             THICKNESS
-                            ]
-    base_tile.add_geom(pos=pos, size=size, rgba = SAND_RGBA)
+    pos =[3 * SQUARE_LENGTH , -(2 - STEP * (1 + 2 * i)) * SQUARE_LENGTH,
+          direction * (2 * i) * THICKNESS]
+    size =  [(2 - (2* STEP * (1+ i))) * SQUARE_LENGTH, SQUARE_LENGTH * 0.1,
+             THICKNESS]
+    base_tile.add_geom(pos=pos, size=size, rgba=BROWN_RGBA)
     # Top
-    pos =[3 * SQUARE_LENGTH ,
-         (2 - STEP * (1 + 2 * i)) * SQUARE_LENGTH,
-         direction * (0 + 2 * i) * THICKNESS]
-    base_tile.add_geom(pos = pos, size = size , rgba = SAND_RGBA)
-  # TODO: Closing
+    pos =[3 * SQUARE_LENGTH , (2 - STEP * (1 + 2 * i)) * SQUARE_LENGTH,
+          direction * (2 * i) * THICKNESS]
+    base_tile.add_geom(pos=pos, size=size , rgba=BROWN_RGBA)
+  # Closing
   l = (2 - STEP * ( 2 * num_stairs)) * SQUARE_LENGTH
-  pos = [ SQUARE_LENGTH * 1 + STEP * (2 + 2 * i) + l,
-           0,
+  pos = [ SQUARE_LENGTH * 1 + STEP * (2 + 2 * i) + l, 0,
           direction * (2 + 2 * i) * THICKNESS]
   size = [l , l, THICKNESS]
-  base_tile.add_geom(pos=pos, size=size, rgba = SAND_RGBA)
+  base_tile.add_geom(pos=pos, size=size, rgba=BROWN_RGBA)
 
-def box_extrusions(base_tile, grid_size=(20, 20),complex = False):
+def debris(spec,name='base_tile'):
+    SQUARE_LENGTH = 1
+    x_offset = SQUARE_LENGTH * 3
+    y_offset = SQUARE_LENGTH * 0
+    BROWN_RGBA = BROWN_RGBA = [0.647, 0.165, 0.165, 1.0]
+
+    # Plane
+    base_tile = spec.body(name)
+    base_tile.add_geom(pos=[x_offset, y_offset, 0],
+                       size = [2 * SQUARE_LENGTH, 2 * SQUARE_LENGTH, 0.1],
+                       rgba = BROWN_RGBA)
+
+    # Debris
+    extrusion = 0.05
+    step = 0.5
+    for i in range(5):
+      for j in range(5):
+        # x y
+        drawing = np.random.normal(size = (4, 2))
+        z = np.zeros((drawing.shape[0], 1))
+        # Add z value
+        base = np.concatenate((drawing, z), axis=1)
+        # Extrude triangle
+        z_extrusion = np.full((drawing.shape[0], 1), extrusion)
+        top = np.concatenate((drawing, z_extrusion), axis=1)
+        mesh = np.vstack((base, top))
+        # Normalize
+        mesh /= np.linalg.norm(mesh, axis=1, keepdims=True)
+
+        # Create Body and add mesh to the Geom of the Body
+        spec.add_mesh(name=f'd{i}_{j}_{name}', uservert=mesh.flatten())
+        pos = [-1 * SQUARE_LENGTH + step * i,
+               -1 * SQUARE_LENGTH + step * j,
+               extrusion * 2]
+
+        body = spec.worldbody.add_body(pos=pos, name=f'd{i}_{j}_{name}', mass=1)
+        body.add_geom(type=mj.mjtGeom.mjGEOM_MESH ,meshname=f'd{i}_{j}_{name}')
+        body.add_freejoint()
+
+def box_extrusions(spec, grid_size=20, complex=False, name='base_tile'):
+  # Warning! complex sometimes leads to creation of holes
+
   SQUARE_LENGTH = 0.1
   x_offset = SQUARE_LENGTH * 11
   y_offset = SQUARE_LENGTH * 19
   x_step = SQUARE_LENGTH * 2
   y_step = -SQUARE_LENGTH * 2
 
+  BROWN_RGBA = [0.647, 0.165, 0.165, 1.0]
+
+  base_tile = spec.body(name)
   # Create initial grid and store geoms ref
-  grid = [[ 0 for _ in range(grid_size[0])] for _ in range(grid_size[0])]
-  for i in range(grid_size[0]):
-    for j in range(grid_size[1]):
+  grid = [[ 0 for _ in range(grid_size)] for _ in range(grid_size)]
+  for i in range(grid_size):
+    for j in range(grid_size):
       rgba = np.random.rand(4)
       rgba[3] = 1
       ref = base_tile.add_geom(
@@ -109,26 +103,24 @@ def box_extrusions(base_tile, grid_size=(20, 20),complex = False):
                y_offset + j * y_step ,
                0],
         size = [0.1, 0.1, 0.1],
-        rgba = SAND_RGBA
+        rgba = BROWN_RGBA
       )
       grid[i][j] = ref
 
-  # Create valid boxes
-  num_boxes = random.randint(4, 50)
-  boxes = []
-  while len(boxes) < num_boxes:
-    start = (random.randint(0, 18), random.randint(0, 18))
-    dim = (random.randint(0, 18), random.randint(0, 18))
-    # Add valid boxes
-    if start[0] + dim [0] < len(grid) and start[1] + dim [1] < len(grid):
-      box = {"start":start , "dim":dim}
-      boxes.append(box)
-
   # Extrude or Cut operation using the boxes
-  for b in boxes:
+  for _ in range(random.randint(4, 50)):
+    box = None
+    while box == None:
+      # Create a box
+      start = (random.randint(0, 18), random.randint(0, 18))
+      dim = (random.randint(0, 18), random.randint(0, 18))
+      # Make suer box is valid
+      if start[0] + dim [0] < len(grid) and start[1] + dim [1] < len(grid):
+        box = {"start":start , "dim":dim}
+    # Use the box to Cut or Extrude
     operation = random.choice([1, -1])
-    start = b["start"]
-    dim = b["dim"]
+    start = box["start"]
+    dim = box["dim"]
     for i in range(start[0], dim[0]):
       for j in range(start[1], dim[1]):
         tile = grid[i][j]
@@ -137,28 +129,30 @@ def box_extrusions(base_tile, grid_size=(20, 20),complex = False):
         else:
           tile.pos[2] = operation * SQUARE_LENGTH
 
-def boxy_terrain(base_tile, grid_size=(20, 20)):
+def boxy_terrain(spec, grid_size=20, name='base_tile'):
   SQUARE_LENGTH = 0.1
   x_step = SQUARE_LENGTH * 2
   y_step = -SQUARE_LENGTH * 2
-
   x_offset = SQUARE_LENGTH * 11
   y_offset = SQUARE_LENGTH * 19
 
+  BROWN_RGBA = [0.647, 0.165, 0.165, 1.0]
 
-  for i in range(grid_size[0]):
-    for j in range(grid_size[1]):
+  base_tile = spec.body(name)
+  for i in range(grid_size):
+    for j in range(grid_size):
       rgba = np.random.rand(4)
       rgba[3] = 1
       base_tile.add_geom(
         pos = [ x_offset + i * x_step ,
                 y_offset + j * y_step ,
-               random.randint(-1, 1) * SQUARE_LENGTH  ],
+               random.randint(-1, 1) * SQUARE_LENGTH ],
         size = [0.1, 0.1, 0.1],
-        rgba = SAND_RGBA
+        rgba = BROWN_RGBA
       )
 
-def tile(spec=None, grid_loc=(0,0), tile_num = 0):
+
+def add_tile(spec=None, grid_loc=(0,0),name='base_tile'):
   if spec is None:
     spec = mj.MjSpec()
 
@@ -170,38 +164,58 @@ def tile(spec=None, grid_loc=(0,0), tile_num = 0):
 
   x = grid_loc[0] * SQUARE_LENGTH * 4
   y = grid_loc[1] * SQUARE_LENGTH * 4
-  body = spec.worldbody.add_body(pos =[x, y, 0])
+  body = spec.worldbody.add_body(pos=[x, y, 0], name=name)
+  center = spec.worldbody.add_body(pos=[x, y, 0])
+  center.add_geom(type = mj.mjtGeom.mjGEOM_SPHERE,size=[0.1,0.1,0])
 
   tile_type = random.randint(0,3)
-  # tile_type = 4
   if tile_type == 0:
-    stairs(base_tile = body, num_stairs = random.randint(2, 8), direction = 1)
+    stairs(spec=spec, num_stairs=random.randint(2, 8),
+           direction=1, name=name)
   elif tile_type == 1:
-    stairs(base_tile = body, num_stairs = random.randint(2, 8), direction = -1)
+    stairs(spec=spec, num_stairs=random.randint(2, 8),
+           direction=-1, name=name)
   elif tile_type == 2:
-    boxy_terrain(base_tile = body)
+    boxy_terrain(spec=spec, name=name)
   elif tile_type == 3:
-    box_extrusions(base_tile = body)
+    box_extrusions(spec=spec, name=name)
+  elif tile_type == 4:
+    debris(spec=spec, name=name)
 
   return spec
 
 if __name__ == "__main__":
 
-  spec =  mj.MjSpec()
-  # tile(spec)
-  tile_num = 0
-  for i in range(-2,2):
-    for j in range(-2,2):
-      tile_num +=1
-      tile(spec = spec, grid_loc=(i,j),tile_num = tile_num)
+  arena_xml = """
+  <mujoco>
+    <visual>
+      <headlight diffuse=".5 .5 .5" specular="1 1 1"/>
+      <global elevation="-10" offwidth="2048" offheight="1536"/>
+      <quality shadowsize="8192"/>
+    </visual>
 
-  pos = [-40, -20, 20 ,40]
-  dir = [-0.8,-0.8, 0.8,0.8]
+    <asset>
+      <texture type="skybox" builtin="gradient" rgb1=".5 .5 .5" rgb2="0 0 0" width="10" height="10"/>
+      <texture type="2d" name="groundplane" builtin="checker" mark="edge" rgb1="1 1 1" rgb2="1 1 1" markrgb="0 0 0" width="300" height="300"/>
+      <material name="groundplane" texture="groundplane" texuniform="true" texrepeat="5 5" reflectance="0.3"/>
+    </asset>
+
+    <worldbody>
+    </worldbody>
+  </mujoco>
+  """
+
+  spec = mj.MjSpec.from_string(arena_xml)
+
 
   # Add lights
-  for x, x_dir in zip(pos, dir):
-    for y, y_dir in zip(pos, dir):
-      spec.worldbody.add_light(pos = [x, y, 5], dir = [-x_dir, -y_dir, -1])
+  for x in [-5, 5]:
+    for y in [-5, 5]:
+      spec.worldbody.add_light(pos = [x, y, 20], dir = [-x, -y, -15])
+
+  for i in range(-2,2):
+    for j in range(-2,2):
+      add_tile(spec=spec, grid_loc=(i, j),name=f'base_tile_{i}_{j}')
 
   model = spec.compile()
   data = mj.MjData(model)
